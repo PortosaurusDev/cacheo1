85274b15cfd6652511680f4b7339aea619664275
PortosaurusBot <claperone.portosaurus@gmail.com>
2020-06-18 21:13:02 +0100

    Auto generated portfolio

---
+++ precache-manifest.e3a26e507ef1087cc2482bff2c0c9ccc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90fdd85346f5438d49b8477412a0fa12",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0d22bc8fc68b270e6b14",
+    "url": "./static/css/2.2c0f828a.chunk.css"
+  },
+  {
+    "revision": "0306b3c166ce955b3dde",
+    "url": "./static/css/main.46cd4fbd.chunk.css"
+  },
+  {
+    "revision": "0d22bc8fc68b270e6b14",
+    "url": "./static/js/2.e302df20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e302df20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0306b3c166ce955b3dde",
+    "url": "./static/js/main.5df380c8.chunk.js"
+  },
+  {
+    "revision": "18c309d16c6c37590b3f",
+    "url": "./static/js/runtime-main.3207836a.js"
+  },
+  {
+    "revision": "cd6d11ef068a3f0d483a61b73044e4ea",
+    "url": "./static/media/defaultProfile.cd6d11ef.png"
+  },
+  {
+    "revision": "4ef8954d49b3a7ab17e7438cb365028b",
+    "url": "./static/media/defaultbanner.4ef8954d.png"
+  }
+]);
